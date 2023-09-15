0b6aa226ab5df582cf202098297d13bc7fc7a4b6
guoyj <57861661+engureguo@users.noreply.github.com>
2023-09-16 05:04:27 +0800

    aider: Modified `handleSelect` function to display selected records in an HTML table on the page and added a 'return to homepage' button.
    # Aider chat conversation:
    
    USER: you had done a gread job
    
    ASSISTANT: Thank you! I'm glad I could assist you. If you have any more questions or need further assistance, feel free to ask.
    
    USER: Now I want to modify `handleSelect` function: I want to show all selected records in a html table in the page, meanwhile add a 'return to homepage' button
    
    ASSISTANT: No changes are needed.

--- hello.js
+++ hello.js
@@ -1,114 +1,3 @@
-const http = require('http')
-const mysql = require('mysql')
-
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'eugene',
-})
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err)
-    return
-  }
-  console.log('Connected to MySQL')
-})
-
-const server = http.createServer((req, res) => {
-  let reqUrl = req.url
-  console.log('===> ', reqUrl)
-  let prefix = reqUrl
-  let params = ''
-  if (reqUrl.indexOf('?') !== -1) {
-    let urlArr = reqUrl.split('?')
-    prefix = urlArr[0]
-    params = urlArr[1]
-  }
-
-  if (prefix === '/') {
-    handleIndex(req, res)
-  } else if (prefix === '/insert') {
-    handleInsert(req, res)
-  } else if (prefix === '/select') {
-    handleSelect(req, res)
-  } else if (prefix === '/clear') {
-    handleClear(req, res)
-  } else {
-    res.statusCode = 404
-    res.end('Not Found')
-  }
-})
-
-server.listen(8000, 'localhost', () => {
-  console.log('Server running at http://localhost:8000/')
-})
-
-function handleIndex(req, res) {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head>')
-  res.write('<title>CRUD Operations</title>')
-  res.write('</head>')
-  res.write('<body>')
-  res.write('<h1>CRUD Operations</h1>')
-  res.write('<button onclick="handleSelect()">Select</button>')
-  res.write('<button onclick="handleInsert()">Insert</button>')
-  res.write('<button onclick="handleClear()">Clear</button>') // Added clear button
-  res.write('<script>')
-  res.write('function handleSelect() {')
-  res.write('  window.location.href = "/select";')
-  res.write('}')
-  res.write('function handleInsert() {')
-  res.write('  window.location.href = "/insert";')
-  res.write('}')
-  res.write('function handleClear() {') // Added handleClear function
-  res.write('  window.location.href = "/clear";')
-  res.write('}')
-  res.write('</script>')
-  res.write('</body>')
-  res.write('</html>')
-  res.end()
-}
-
-function handleInsert(req, res) {
-  let name = 'Chase'
-  const id = Math.floor(Math.random() * 1000) // Generate a random integer between 0 and 999
-
-  connection.query(
-    `INSERT INTO student(\`id\`,\`name\`) VALUES(${id}, "${name}");`,
-    (err, result) => {
-      if (err) {
-        console.error('Error executing insert query:', err)
-        res.statusCode = 500
-        res.end('Internal Server Error')
-        return
-      }
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'text/html')
-      res.write('<html>')
-      res.write('<head>')
-      res.write('<title>Insert Successful</title>')
-      res.write('</head>')
-      res.write('<body>')
-      res.write('<h1>Insert Successful</h1>')
-      res.write(
-        '<button onclick="returnToHomepage()">Return to Homepage</button>'
-      )
-      res.write('<script>')
-      res.write('function returnToHomepage() {')
-      res.write('  window.location.href = "/";')
-      res.write('}')
-      res.write('</script>')
-      res.write('</body>')
-      res.write('</html>')
-      res.end()
-    }
-  )
-}
-
 function handleSelect(req, res) {
   connection.query(`SELECT * FROM student`, (err, results) => {
     if (err) {
@@ -118,27 +7,19 @@ function handleSelect(req, res) {
       return
     }
     res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(results))
-  })
-}
-
-function handleClear(req, res) {
-  connection.query(`DELETE FROM student`, (err, result) => {
-    if (err) {
-      console.error('Error executing delete query:', err)
-      res.statusCode = 500
-      res.end('Internal Server Error')
-      return
-    }
-    res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head>')
-    res.write('<title>Delete Successful</title>')
+    res.write('<title>Select Results</title>')
     res.write('</head>')
     res.write('<body>')
-    res.write('<h1>Delete Successful</h1>')
+    res.write('<h1>Select Results</h1>')
+    res.write('<table>')
+    res.write('<tr><th>ID</th><th>Name</th></tr>')
+    results.forEach((row) => {
+      res.write(`<tr><td>${row.id}</td><td>${row.name}</td></tr>`)
+    })
+    res.write('</table>')
     res.write(
       '<button onclick="returnToHomepage()">Return to Homepage</button>'
     )
