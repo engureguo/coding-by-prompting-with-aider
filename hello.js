da1d4764541079c8500c749ebb9d3f97c7f92688
guoyj <57861661+engureguo@users.noreply.github.com>
2023-09-16 03:56:32 +0800

    aider: Added MySQL connection and performed CRUD operations using the connection in hello.js.
    # Aider chat conversation:
    
    USER: then, how to connect mysql in hello.js?
    
    ASSISTANT: No changes are needed.

--- hello.js
+++ hello.js
@@ -1,9 +1,38 @@
 const http = require('http');
+const mysql = require('mysql');
+
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database'
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  // Perform CRUD operations using the connection
+
+  // Example: Select all records from a table
+  connection.query('SELECT * FROM your_table', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+      return;
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(results));
+  });
 });
 
 server.listen(8000, 'localhost', () => {
